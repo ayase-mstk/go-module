334e9c4aaf42d1b46f5490f7e9c39f10aab0313c
ayase-mstk <masa1215.hayase@gmail.com>
2024-07-06 01:02:18 +0900

    :zap: add IsPrime

---
+++ go02/ex06/vendor/piscine/isprime.go
@@ -0,0 +1,32 @@
+package piscine
+
+func sqrt(nb int) int {
+	if nb <= 0 {
+		return 0
+	}
+
+	left, right := 1, nb
+	var ans int
+	for left <= right {
+		mid := (left + right) / 2
+		if mid*mid <= nb {
+			ans = mid
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	return ans
+}
+
+func IsPrime(nb int) bool {
+	if nb <= 2 {
+		return false
+	}
+	for i := 2; i <= sqrt(nb); i++ {
+		if nb%i == 0 {
+			return false
+		}
+	}
+	return true
+}
