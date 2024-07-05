9d632ab6cbd339c1eefcd011293e1535684ef840
ayase-mstk <masa1215.hayase@gmail.com>
2024-07-06 01:47:09 +0900

    :recycle: refactoring IsPrime

--- go02/ex07/vendor/piscine/findNextPrime.go
+++ go02/ex07/vendor/piscine/findNextPrime.go
@@ -1,29 +1,10 @@
 package piscine
 
-func sqrt(nb int) int {
-	if nb <= 0 {
-		return 0
-	}
-
-	left, right := 1, nb
-	var ans int
-	for left <= right {
-		mid := (left + right) / 2
-		if mid*mid <= nb {
-			ans = mid
-			left = mid + 1
-		} else {
-			right = mid - 1
-		}
-	}
-	return ans
-}
-
 func isPrime(nb int) bool {
 	if nb <= 2 {
 		return false
 	}
-	for i := 2; i <= sqrt(nb); i++ {
+	for i := 2; i*i <= nb; i++ {
 		if nb%i == 0 {
 			return false
 		}
