f4f73f244b589adaa287f47f2780fadba48e0964
ayase-mstk <masa1215.hayase@gmail.com>
2024-07-08 20:12:41 +0900

    :bug: word is alphanumeric

--- go03/ex07/main.go
+++ go03/ex07/main.go
@@ -7,4 +7,5 @@ import (
 
 func main() {
 	fmt.Println(piscine.Capitalize("Hello! How are you? How+are+things+4you?"))
+	fmt.Println(piscine.Capitalize("7ello! Hoあw aいre y◎ou? How+are+things+4you?"))
 }
--- go03/ex07/vendor/piscine/capitalize.go
+++ go03/ex07/vendor/piscine/capitalize.go
@@ -16,14 +16,18 @@ func isUpperCase(r rune) bool {
 	return 'A' <= r && r <= 'Z'
 }
 
-func isAlphabet(r rune) bool {
-	return isLowerCase(r) || isUpperCase(r)
+func isNumeric(r rune) bool {
+	return '0' <= r && r <= '9'
+}
+
+func isAlphaNumeric(r rune) bool {
+	return isLowerCase(r) || isUpperCase(r) || isNumeric(r)
 }
 
 func Capitalize(s string) string {
 	var sl []rune = []rune(s)
 	for i := 0; i < sliceLen(sl); i++ {
-		if i == 0 || (i != 0 && !isAlphabet(sl[i-1])) {
+		if i == 0 || (i != 0 && !isAlphaNumeric(sl[i-1])) {
 			if isLowerCase(sl[i]) {
 				sl[i] = sl[i] - ('a' - 'A')
 			}
