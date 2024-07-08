6ae2225c298376295c8dff19e73bbfe1d769ad6b
ayase-mstk <masa1215.hayase@gmail.com>
2024-07-08 14:25:41 +0900

    :zap: modify find next prime

--- go02/ex07/main.go
+++ go02/ex07/main.go
@@ -6,7 +6,8 @@ import (
 )
 
 func main() {
-	fmt.Println(piscine.FindNextPrime(5))
+	fmt.Println(piscine.FindNextPrime(-1))
 	fmt.Println(piscine.FindNextPrime(4))
+	fmt.Println(piscine.FindNextPrime(5))
 	fmt.Println(piscine.FindNextPrime(48))
 }
--- go02/ex07/vendor/piscine/findNextPrime.go
+++ go02/ex07/vendor/piscine/findNextPrime.go
@@ -1,11 +1,18 @@
 package piscine
 
 func isPrime(nb int) bool {
-	if nb <= 2 {
+	if nb <= 1 {
 		return false
 	}
-	for i := 2; i*i <= nb; i++ {
-		if nb%i == 0 {
+	if nb <= 3 {
+		return true
+	}
+	if nb%2 == 0 || nb%3 == 0 {
+		return false
+	}
+
+	for i := 5; i*i <= nb; i += 6 { // i = 6n - 1でloop
+		if nb%i == 0 || nb%(i+2) == 0 { // 6n-1と6n+1で計算する
 			return false
 		}
 	}
