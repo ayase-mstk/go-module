1da865c595e7ee4c340f8d55be54cb9a9d47e8ba
ayase-mstk <masa1215.hayase@gmail.com>
2024-07-08 20:00:57 +0900

    :recycle: refactor

--- go03/ex04/main.go
+++ go03/ex04/main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"fmt"
 	"piscine"
+	//"strings"
 )
 
 func main() {
 	fmt.Println(piscine.Compare("Hello!", "Hello!"))
+	//fmt.Println(strings.Compare("Hello!", "Hello!"))
 	fmt.Println(piscine.Compare("Salut!", "lut!"))
+	//fmt.Println(strings.Compare("Salut!", "lut!"))
 	fmt.Println(piscine.Compare("Ola!", "Ol"))
+	//fmt.Println(strings.Compare("Ola!", "Ol"))
+	fmt.Println(piscine.Compare("おはよう", "おは"))
+	//fmt.Println(strings.Compare("おはよう", "おは"))
 }
