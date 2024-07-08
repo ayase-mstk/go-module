a19bba595d898703996ee7605a5a5a3c9875a921
ayase-mstk <masa1215.hayase@gmail.com>
2024-07-08 19:58:06 +0900

    :bug: Index by bytes

--- go03/ex03/vendor/piscine/index.go
+++ go03/ex03/vendor/piscine/index.go
@@ -1,27 +1,36 @@
 package piscine
 
+func strLen(s string) int {
+	bytes := []byte(s)
+	l := 0
+	for range bytes {
+		l++
+	}
+	return l
+}
+
 func Index(s string, toFind string) int {
-	n := len(toFind)
+	n := strLen(toFind)
 
 	switch {
 	case n == 0:
 		return 0
 	case n == 1:
-		for i := 0; i < len(s); i++ {
+		for i := 0; i < strLen(s); i++ {
 			if s[i] == toFind[0] {
 				return i
 			}
 		}
 		return -1
-	case n == len(s):
+	case n == strLen(s):
 		if s == toFind {
 			return 0
 		}
 		return -1
-	case n > len(s):
+	case n > strLen(s):
 		return -1
 	default:
-		for i := 0; i < len(s)-n; i++ {
+		for i := 0; i < strLen(s)-n; i++ {
 			if s[i:i+n] == toFind {
 				return i
 			}
