4a84b6e412ace8b3a120508bde9392536da300e3
ayase-mstk <masa1215.hayase@gmail.com>
2024-07-08 19:05:44 +0900

    :zap: add sliceLen

--- go03/ex01/vendor/piscine/nrune.go
+++ go03/ex01/vendor/piscine/nrune.go
@@ -1,9 +1,17 @@
 package piscine
 
+func sliceLen(sl []rune) int {
+  l := 0
+  for range sl {
+    l++
+  }
+  return l
+}
+
 func NRune(s string, n int) rune {
 	var ret []rune = []rune(s)
 
-	if n <= 0 || len(ret) < n {
+	if n <= 0 || sliceLen(ret) < n {
 		return 0
 	}
 
