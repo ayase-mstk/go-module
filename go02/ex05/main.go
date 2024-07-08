3fc71c9fea88c0d7e4cb372f9999b2bdeb73198c
ayase-mstk <masa1215.hayase@gmail.com>
2024-07-08 22:23:00 +0900

    :zap: modify sqrt

--- go02/ex05/main.go
+++ go02/ex05/main.go
@@ -6,10 +6,14 @@ import (
 )
 
 func main() {
-	fmt.Println(piscine.Sqrt(4))
-	fmt.Println(piscine.Sqrt(3))
-	fmt.Println(piscine.Sqrt(15 * 15))
-	fmt.Println(piscine.Sqrt(42 * 42))
+	fmt.Println(piscine.Sqrt(-1))
 	fmt.Println(piscine.Sqrt(0))
-	fmt.Println(piscine.Sqrt(-9))
+	fmt.Println(piscine.Sqrt(1))
+	fmt.Println(piscine.Sqrt(2))
+	fmt.Println(piscine.Sqrt(3))
+	fmt.Println(piscine.Sqrt(4))
+	fmt.Println(piscine.Sqrt(5))
+	fmt.Println(piscine.Sqrt(42 * 42))
+	fmt.Println(piscine.Sqrt(3037000499.0 * 3037000499.0))
+	//fmt.Println(piscine.Sqrt(3037000500 * 3037000499))
 }
--- go02/ex05/vendor/piscine/sqrt.go
+++ go02/ex05/vendor/piscine/sqrt.go
@@ -2,9 +2,10 @@ package piscine
 
 func Sqrt(nb int) int {
 	ret := 0
-	for i := 1; i*i <= nb; i++ {
-		if i*i == nb {
+	for i := 1; i <= nb/i; i++ {
+		if i == nb/i && nb%i == 0 {
 			ret = i
+			break
 		}
 	}
 	return ret
