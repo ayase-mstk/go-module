541641c8575f3be2f3202ac801b0d147816cb517
ayase-mstk <masa1215.hayase@gmail.com>
2024-07-10 00:17:30 +0900

    :zap: modify fibonacci more efficientry

--- go02/ex04/main.go
+++ go02/ex04/main.go
@@ -14,6 +14,8 @@ func main() {
 	fmt.Println(piscine.Fibonacci(arg1)) // 3
 	arg1 = 16
 	fmt.Println(piscine.Fibonacci(arg1)) // 987
+	arg1 = 92
+	fmt.Println(piscine.Fibonacci(arg1)) // Max
 }
 
 // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711
--- go02/ex04/vendor/piscine/fibonacci.go
+++ go02/ex04/vendor/piscine/fibonacci.go
@@ -1,14 +1,15 @@
 package piscine
 
+func fib(n int, a int, b int) int {
+	if n == 0 {
+		return b
+	}
+	return fib(n-1, b, a+b)
+}
+
 func Fibonacci(index int) int {
 	if index < 0 {
 		return -1
 	}
-	if index == 0 {
-		return 0
-	}
-	if index == 1 {
-		return 1
-	}
-	return Fibonacci(index-2) + Fibonacci(index-1)
+	return fib(index, 1, 0)
 }
